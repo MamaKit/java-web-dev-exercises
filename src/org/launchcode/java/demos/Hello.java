630acef50c21ae58cb079e73f56b5d914c1fe158
MamaKit <mama.kit.pyles@gmail.com>
2022-09-07 19:36:00 -0500

    Testing the connection.

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -6,5 +6,6 @@ package org.launchcode.java.demos;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+//        I needed a change to commit & test the connection to GitHub.
     }
 }
